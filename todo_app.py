2d432f2d6efcdd2a727d1f30e9df2615ff2d3832
Dranhoc <160327375+Dranhoc@users.noreply.github.com>
2024-08-12 15:00:30 +0200

    init db

--- todo_app.py
+++ todo_app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
+DB_NAME = 'todo.db'
 
-items = [
-    'Buy Food',
-    'Go to Gym',
-    'Watch Movie'
-]
+
+def create_database():
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, task TEXT)''')
+    conn.commit()
+    conn.close()
 
 
 @app.route('/')
 def index():
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('SELECT id, task FROM items')
+    items = c.fetchall()
+    conn.close()
     return render_template('items.j2', items=items)
 
 
@@ -19,21 +28,48 @@ def index():
 def create():
     if request.method == 'POST':
         todo_item = request.form['new_item']
-        items.append(todo_item)
-        return render_template('items.j2', items=items)
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute('INSERT INTO items (task) VALUES (?)', (todo_item,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
     return render_template('create.j2')
+
+
+def get_item(id):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    item = c.execute('SELECT * FROM items WHERE id = (?)', (id,)).fetchone()
+    conn.close()
+    return item
 
 
 @app.route('/update/<int:item_id>', methods=('GET', 'POST'))
 def update(item_id):
+    item = get_item(item_id)
     if request.method == 'POST':
         updated_item = request.form['update_item']
-        items[item_id] = updated_item
-        return render_template('items.j2', items=items)
-    return render_template('update.j2', item=items[item_id], item_id=item_id)
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute('UPDATE items SET task=(?) WHERE id = (?)', (updated_item, item_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('update.j2', item=item, item_id=item_id)
 
 
 @app.route('/delete/<int:item_id>', methods=('GET', 'POST'))
 def delete(item_id):
-        items.pop(item_id)
-        return render_template('items.j2', items=items)
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('DELETE FROM items WHERE id = ?', (item_id,))
+    conn.commit()
+    items = c.execute('SELECT * FROM items').fetchall()
+    conn.close()
+    return render_template('items.j2', items=items)
+
+
+if __name__ == '__main__':
+    create_database()
+    app.run(debug=True)
